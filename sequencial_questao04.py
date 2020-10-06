1f2bf3a122d5d05dbac8b348774acf2261fca675
Prof. João Paulo <62918228+profjoaopaulo@users.noreply.github.com>
2020-10-06 00:12:50 -0300

    Add files via upload

---
+++ sequencial_questao04.py
@@ -0,0 +1,12 @@
+#https://wiki.python.org.br/EstruturaSequencial 
+#Questão 04
+
+n1 = float( input("Digite nota 1: ") ) 
+n2 = float( input("Digite nota 2: ") ) 
+n3 = float( input("Digite nota 3: ") ) 
+n4 = float( input("Digite nota 4: ") ) 
+
+media = ( n1 + n2 + n3 + n4 ) / 4
+
+print('Média é igual a %.1f' % media)
+
